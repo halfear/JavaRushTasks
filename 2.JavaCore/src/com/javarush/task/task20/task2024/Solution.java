3c8d4b84b572750d49deb0c49c724ae53638dbc4
halfear <halfear@mail.ru>
2020-09-02 23:09:31 +0300

    Solved Tasks 23-24 Level 20

---
+++ 2.JavaCore/src/com/javarush/task/task20/task2024/Solution.java
@@ -0,0 +1,23 @@
+package com.javarush.task.task20.task2024;
+
+import java.io.*;
+import java.util.LinkedList;
+import java.util.List;
+
+/* 
+Знакомство с графами
+*/
+public class Solution implements Serializable {
+    int node;
+    List<Solution> edges = new LinkedList<>();
+
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        Solution solution = new Solution();
+
+        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("somefile.bin"));
+        oos.writeObject(solution);
+
+        ObjectInputStream ois = new ObjectInputStream(new FileInputStream("somefile.bin"));
+        Solution newSolution = (Solution) ois.readObject();
+    }
+}
